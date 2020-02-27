8f3d548f5d16bafa0f08a4d90370789371fd8691
Kia Morelos <kmorelos@bti-usa.com>
2020-02-27 10:09:38 -0700

    added desroy action and partials. decluttered code, added link to blog page to home and about pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
 def index
   @articles = Article.all
-  
+
 end
 
 def new
@@ -34,6 +34,13 @@ end
 
 def show
   @article = Article.find(params[:id])
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article was successfully deleted."
+  redirect_to articles_path
 end
 
   private
